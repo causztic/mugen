7b59982789b338f513c80c794b11bbaf28415d0b
causztic <limyaojie93@gmail.com>
2015-07-25 03:11:04 +0800

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :default_env, {
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml .env}
+# set :linked_files, %w{config/database.yml .env}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
