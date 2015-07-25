98ff9c3015265648e2639feed7dac5f1325aa502
causztic <limyaojie93@gmail.com>
2015-07-25 11:22:09 +0800

    ~

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -34,6 +34,5 @@ end
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
 before_exec do |_|
-  puts ENV['AWS_REGION']
   ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
 end
