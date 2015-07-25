b3e3042d8321defcd07bf40b08e409dfeb67760e
causztic <limyaojie93@gmail.com>
2015-07-25 10:45:44 +0800

    + music to play on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,15 @@
+require 'aws-sdk-resources'
+require 'net/http'
+
 class MainController < ApplicationController
 
   def landing
+    s3 = Aws::S3::Resource.new
+    obj = s3.bucket('grafiore').object('鳥の詩.mp3')
+    url = URI.parse(obj.presigned_url(:get))
+
+    @src = !Rails.env.development? ? "" : url
+    #s3.get_object({bucket: "grafiore", key: "鳥の詩.mp3"})
   end
 
 end
