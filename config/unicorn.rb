d0acecb30fc61ba04af1f01c569a75e73294bca9
causztic <limyaojie93@gmail.com>
2015-07-25 08:30:07 +0800

    ~ unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,38 @@
-# workers
-worker_processes 3
+root = "/home/deployer/apps/mugen/current"
+working_directory root
 
-# use correct Gemfile on restarts
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
+pid "#{root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+worker_processes 3
+timeout 30
+preload_app true
+
+listen '/tmp/unicorn.spui.sock', backlog: 64
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
-
 after_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
 end
