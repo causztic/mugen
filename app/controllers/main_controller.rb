05483863bd5f9dc455c7ce5614c77570e3d784e5
causztic <limyaojie93@gmail.com>
2015-08-06 15:20:10 +0800

    + album image, + multiple to production

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,15 +4,15 @@ require 'net/http'
 class MainController < ApplicationController
 
   def landing
-    s3 = Aws::S3::Resource.new
-    obj = s3.bucket('grafiore').object('鳥の詩.mp3')
-    url = URI.parse(obj.presigned_url(:get))
-    
     if Rails.env.development?
       #@src = ["/assets/会いたい.mp3", "/assets/tori-no-uta.mp3"]
       @abs_src = ["app/assets/audios/会いたい.mp3", "app/assets/audios/tori-no-uta.mp3"]
     else
-      @abs_src = [url]
+      s3 = Aws::S3::Resource.new
+      @abs_src = []
+      s3.bucket('grafiore').objects.each do |music|
+        @abs_src << URI.parse(music.presigned_url(:get))
+      end
     end
 
     @music = Music.create_music_files(@abs_src)
