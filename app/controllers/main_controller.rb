91e3f5e802d7704b28a26ec5a69f25646f868536
causztic <limyaojie93@gmail.com>
2015-08-06 15:27:05 +0800

    comment out s3 for now

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,16 +4,16 @@ require 'net/http'
 class MainController < ApplicationController
 
   def landing
-    if Rails.env.development?
+    #if Rails.env.development?
       #@src = ["/assets/会いたい.mp3", "/assets/tori-no-uta.mp3"]
       @abs_src = ["app/assets/audios/会いたい.mp3", "app/assets/audios/tori-no-uta.mp3"]
-    else
-      s3 = Aws::S3::Resource.new
-      @abs_src = []
-      s3.bucket('grafiore').objects.each do |music|
-        @abs_src << URI.parse(music.presigned_url(:get))
-      end
-    end
+    #else
+    #  s3 = Aws::S3::Resource.new
+    #  @abs_src = []
+    #  s3.bucket('grafiore').objects.each do |music|
+    #    @abs_src << URI.parse(music.presigned_url(:get))
+    #  end
+    #end
 
     @music = Music.create_music_files(@abs_src)
 
