82b8d6179595de9916122207857f9d5fbad90bf1
causztic <limyaojie93@gmail.com>
2015-11-07 22:18:25 +0800

    set music target from precompiled file on production

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,13 @@ require 'net/http'
 class MainController < ApplicationController
 
   def landing
-    #if Rails.env.development?
     @abs_src = Dir.glob('app/assets/audios/*').select {|f| File.file? f}
     @music = Music.create_music_files(@abs_src)
+    if Rails.env.production? 
+      @music = @music.each {|m| m.path = view_context.asset_path(m.path)}
+    else
+      @music = @music.each {|m| m.path = "/assets/" + m.path }
+    end
     gon.music = @music
   end
 
