1e47243ec6959f49f05d460fe1b268f078be9484
causztic <limyaojie93@gmail.com>
2015-11-07 21:30:48 +0800

    set unicorn_env to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,8 @@ set :port, 22
 set :user, 'graf'
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :rails_env, "production"
+set :unicorn_env, "production"
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 
 server '103.253.146.211',
