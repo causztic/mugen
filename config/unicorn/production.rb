38aa030a60732a56b445c0293575e92b8675e2d9
causztic <limyaojie93@gmail.com>
2015-11-07 23:39:35 +0800

    reduce workers to 2

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,7 +6,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-worker_processes 3
+worker_processes 2
 timeout 30
 preload_app true
 
