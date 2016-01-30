cc3c326bf0d968912e454d3ec23fa3d2f807c20d
yaojie <limyaojie93@gmail.com>
2016-01-30 10:34:15 +0800

    change ip and user used for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
 set :port, 22
-set :user, 'graf'
+set :user, 'pi'
 set :deploy_via, :remote_cache
 set :use_sudo, false
 set :rails_env, "production"
 set :unicorn_env, "production"
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 
-server '103.253.146.211',
+server '192.168.1.207',
   roles: [:web, :app],
   port: fetch(:port),
   user: fetch(:user),
@@ -18,5 +18,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w(publickey),
-  user: 'graf',
+  user: fetch(:user)
 }
