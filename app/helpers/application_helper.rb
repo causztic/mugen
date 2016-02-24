b4ad855193116d1a9a64638c256c7f4c268e3317
yaojie <limyaojie93@gmail.com>
2016-02-24 22:17:21 +0800

    updated some content for about me :

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	
+	DOB = "06-08-1993".to_date
+
+	def current_age
+		now = Time.now.utc.to_date
+		now.year - DOB.year - ((now.month > DOB.month || (now.month == DOB.month && now.day >= DOB.day)) ? 0 : 1)
+	end
+
 end
