ebb4b748c1d071aec0fdec42046545de013ff37a
yaojie <limyaojie93@gmail.com>
2016-02-24 17:06:13 +0800

    set rvm type to user

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,7 @@ set :branch, "master"
 set :use_sudo, false
 set :bundle_binstubs, nil
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :rvm1_ruby_version, "2.3.0@mugen"
-set :rvm_type, :system # :user if RVM installed in $HOME
+set :rvm_type, :user # :user if RVM installed in $HOME
 
 before 'deploy', 'rvm1:alias:create'
 before 'deploy', 'rvm1:install:gems' 
