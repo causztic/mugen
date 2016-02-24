1f3aafe9d290df04c3672b8867079510e9bf442d
yaojie <limyaojie93@gmail.com>
2016-02-24 20:59:44 +0800

    install react components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require turbolinks//= require react
+//= require react_ujs
+//= require components
+
 //= require_tree .
 
